f1873a0b5069170c3b983ab4e63f4c0e1cf288e9
HappyHappy1996 <semenrikirovi4@gmail.com>
2015-09-29 18:21:11 +0300

    deleted classpath and jar

--- src/ua/nagib/Launcher.java
+++ src/ua/nagib/Launcher.java
@@ -2,12 +2,16 @@ package ua.nagib;
 
 import java.io.IOException;
 
+import javax.swing.JComboBox;
+
 import ua.nagib.GUI.LoginFrame;
 
 public class Launcher {
 
 	public static void main(String[] args) throws IOException {
+		
 		new LoginFrame();
+		
 	}
 
 }
--- src/ua/nagib/db/DBWorker.java
+++ src/ua/nagib/db/DBWorker.java
@@ -7,40 +7,38 @@ import java.sql.SQLException;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
-
 public class DBWorker {
 
- private final String URL = "jdbc:mysql://localhost:3306/ratedb";
- private final String USER = "root";
- private final String PASSWORD = "root";
+	private static final String URL = "jdbc:mysql://localhost:3306/ratedb";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
 
- private static DBWorker instance;
- private Connection connection;
+	private static DBWorker instance;
+	private Connection connection;
 
- private DBWorker() throws SQLException {
-  try {
-   System.out.println("1");
-   Driver driver = new FabricMySQLDriver();
-   System.out.println("2");
-   DriverManager.registerDriver(driver);
-   System.out.println("3");
-   connection = DriverManager.getConnection(URL, USER, PASSWORD);
-   System.out.println("4");
-  } catch (SQLException e) {
-   connection = null;
-   System.err.println("Couldn't get connection");
-  }
- }
+	private DBWorker() throws SQLException {
+		try {
+			System.out.println("1");
+			Driver driver = new FabricMySQLDriver();
+			System.out.println("2");
+			DriverManager.registerDriver(driver);
+			System.out.println("3");
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			connection = null;
+			System.err.println("Couldn't get connection");
+		}
+	}
 
- public Connection getConnection() {
-  return connection;
- }
+	public Connection getConnection() {
+		return connection;
+	}
 
- public static synchronized DBWorker getInstance() throws SQLException {
-  if (instance == null) {
-   instance = new DBWorker();
-  }
-  return instance;
- }
+	public static synchronized DBWorker getInstance() throws SQLException {
+		if (instance == null) {
+			instance = new DBWorker();
+		}
+		return instance;
+	}
 
 }
