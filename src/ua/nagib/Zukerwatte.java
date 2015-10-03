b5439513cb419e960f1763f76448014fb70d144e
HappyHappy1996 <semenrikirovi4@gmail.com>
2015-10-03 14:11:20 +0300

    NO MAN NO CRY

--- src/ua/nagib/Zukerwatte.java
+++ src/ua/nagib/Zukerwatte.java
@@ -2,8 +2,14 @@ package ua.nagib;
 
 public class Zukerwatte {
 
+	private static String staticValue = "NO MAN NO CRY";
+
 	public static void sayOliver() {
 		System.out.println("Oliver");
+	}
+	
+	public static void print() {
+		System.out.println(staticValue );
 	}
 	
 }
